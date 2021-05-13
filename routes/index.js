a5b64a7081ace0e6ef38acd5ec3e4e74ea6abc22
root <inseo8070@naver.com>
2021-05-13 08:17:08 +0000

    [feat] mongoDB Schema 생성 및 연동 확인

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Store = require('../models/store');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {
+	  const stores = await Store.find({});
+	  console.log(stores);
+	  res.json(stores);
+	  // res.render('index', { title: 'Express' });
+  } catch (err) {
+	  console.error(err);
+	  next(err);
+  }
 });
 
 module.exports = router;
